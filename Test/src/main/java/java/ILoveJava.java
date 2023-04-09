42388098142cfc59327743e07eed836eca01fb43
Beggie98 <bjuraev2020@gmail.com>
2023-04-09 21:02:10 +0500

    Updated a java class

--- Test/src/main/java/java/ILoveJava.java
+++ Test/src/main/java/java/ILoveJava.java
@@ -3,5 +3,6 @@ package Test.src.main.java.java;
 public class ILoveJava {
     public static void main(String[] args) {
         System.out.println("I love Java");
+        System.out.println("I love Java too");
     }
 }
